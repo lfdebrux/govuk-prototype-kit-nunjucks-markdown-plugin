dd2f0741be89d41d7cb2e9db9f53625726b7731d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-08-18 10:37:21 +0100

    Update package name in setup message

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+var path = require('path');
+
 var Markdown = require('./lib/markdown_tag');
+
+packageName = function() {
+  var segments = __dirname.split(path.sep)
+  var index = segments.lastIndexOf('node_modules')
+  if (index == -1) {
+    return segments[segments.length - 1]
+  }
+  if (segments[index + 1][0] == '@') {
+    return [segments[index + 1], segments[index + 2]].join('/')
+  } else {
+    return segments[index + 1]
+  }
+}
 
 module.exports.register = function(env, renderMarkdown) {
   env.addExtension('markdown', new Markdown(env, renderMarkdown));
@@ -9,7 +24,7 @@ module.exports.setupPlugin = function(renderMarkdown) {
   return function (req, res, next) {
     if (setupDone) return next();
 
-    console.log('@lfdebrux/nunjucks-markdown: doing one time setup');
+    console.log('%s doing one time setup', packageName());
     module.exports.register(req.app.get('nunjucksEnv'), renderMarkdown);
     setupDone = true;
     next();
